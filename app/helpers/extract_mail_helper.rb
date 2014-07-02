3e9cb0fceb28429cf42b227c0e26f5889e8031c7
almazhou <zx_new@126.com>
2014-07-02 14:02:31 +0800

    add method for extract day

--- app/helpers/extract_mail_helper.rb
+++ app/helpers/extract_mail_helper.rb
@@ -6,13 +6,18 @@ module ExtractMailHelper
 	end
 
 	def self.extract_leave_amount(email_subject_string)
-		substrings = split_name(email_subject_string)
-
-		amount_string = substrings[1].split("of")
+		amount_string = split_day(email_subject_string)
 		return amount_string[0].strip
 	end
 
 	def self.split_name(email_subject_string)
 		email_subject_string.split("will take")
+	end
+
+	def self.split_day(email_subject_string)
+		substrings = split_name(email_subject_string)
+
+		amount_string = substrings[1].split("of")
+		return amount_string
 	end
 end
